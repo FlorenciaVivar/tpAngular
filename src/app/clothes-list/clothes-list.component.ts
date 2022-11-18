66835851e3bc2a46f43ce6f5d91793c81af8ff9f
Florencia Vivar <daianavivar93@gmail.com>
2022-11-18 18:08:50 -0300

    se agrega componente clothes-list

--- src/app/clothes-list/clothes-list.component.ts
+++ src/app/clothes-list/clothes-list.component.ts
@@ -3,17 +3,33 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-clothes-list',
   templateUrl: './clothes-list.component.html',
-  styleUrls: ['./clothes-list.component.css']
+  styleUrls: ['./clothes-list.component.scss']
 })
 export class ClothesListComponent implements OnInit {
 
-  clothes = {
+  clothesSantorini = {
     "name" :"W's Santorini Island",
     "type":"Remera",
     "price":"15000",
     "stock":"15",
     "image":"assets/img/santorini.jpg"
   }
+  clothesCreta = {
+    "name" :"W's Creta Island",
+    "type":"Pantalon",
+    "price":"35000",
+    "stock":"5",
+    "image":"assets/img/creta.jpg"
+  }
+
+  clothesHvar = {
+    "name" :"W's Hvar Logo Hat",
+    "type":"Gorra",
+    "price":"12500",
+    "stock":"10",
+    "image":"assets/img/hvar.jpg"
+  }
+
   constructor() { }
 
   ngOnInit(): void {
